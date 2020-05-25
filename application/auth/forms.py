c505da50393eff5b8c986de5d9fec71cdd03edc6
tire95 <timo.reijonen@helsinki.fi>
2020-05-25 18:04:04 +0300

    Modify validators for forms

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, validators
   
 class LoginForm(FlaskForm):
-    username = StringField("Username", [validators.Length(min=2, max=20)])
-    password = PasswordField("Password")
+    username = StringField("Username", [validators.InputRequired(), validators.Length(min=2, max=20)])
+    password = PasswordField("Password", [validators.InputRequired(), validators.InputRequired(message="Password required")])
   
     class Meta:
         csrf = False
--- application/campaigns/forms.py
+++ application/campaigns/forms.py
@@ -2,15 +2,15 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, validators
   
 class CampaignForm(FlaskForm):
-    name = StringField("Campaign's name", [validators.Length(min=2, max=50)])
-    game_system = StringField("Game system (e.g. D&D 5e, Pathfinder)", [validators.Length(min=2, max=50)])
+    name = StringField("Campaign's name", [validators.InputRequired(), validators.Length(min=2, max=50)])
+    game_system = StringField("Game system (e.g. D&D 5e, Pathfinder)", [validators.InputRequired(), validators.Length(min=2, max=50)])
     password = PasswordField("Campaign's password")
   
     class Meta:
         csrf = False
 
 class RegisterForm(FlaskForm):
-    password = PasswordField("Campaign's password", [validators.Length(min=1)])
+    password = PasswordField("Campaign's password", [validators.InputRequired()])
   
     class Meta:
         csrf = False        
