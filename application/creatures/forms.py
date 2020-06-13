a0c0b393ce6ffba87b811e8055076a0358445808
tire95 <timo.reijonen@helsinki.fi>
2020-06-13 15:06:38 +0300

    Add labels for input fields

--- application/creatures/forms.py
+++ application/creatures/forms.py
@@ -2,17 +2,17 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, validators
 
 class CreatureForm(FlaskForm):
-    name = StringField("Creature name", [validators.InputRequired(), validators.Length(min=2, max=144)], render_kw={"placeholder": "Creature name"})
-    type = StringField("Creature type", [validators.InputRequired(), validators.Length(min=2, max=144)], render_kw={"placeholder": "Creature type"})
-    size = StringField("Creature size", [validators.InputRequired(), validators.Length(min=2, max=144)], render_kw={"placeholder": "Creature size"})
+    name = StringField("Creature name", [validators.InputRequired(), validators.Length(min=2, max=144)], render_kw={"placeholder": "Creature's name"})
+    type = StringField("Creature type", [validators.InputRequired(), validators.Length(min=2, max=144)], render_kw={"placeholder": "Creature's type"})
+    size = StringField("Creature size", [validators.InputRequired(), validators.Length(min=2, max=144)], render_kw={"placeholder": "Creature's size"})
     description = TextAreaField("Description about the creature", [validators.Length(max=1000)], render_kw={"placeholder": "Description about the creature"})
  
     class Meta:
         csrf = False
 
 class ModifyForm(FlaskForm):
-    type = StringField("Creature type", [validators.InputRequired(), validators.Length(min=2, max=144)], render_kw={"placeholder": "Creature type"})
-    size = StringField("Creature size", [validators.InputRequired(), validators.Length(min=2, max=144)], render_kw={"placeholder": "Creature size"})
+    type = StringField("Creature type", [validators.InputRequired(), validators.Length(min=2, max=144)], render_kw={"placeholder": "Creature's type"})
+    size = StringField("Creature size", [validators.InputRequired(), validators.Length(min=2, max=144)], render_kw={"placeholder": "Creature's size"})
     description = TextAreaField("Description about the creature", [validators.Length(max=1000)], render_kw={"placeholder": "Description about the creature"})
  
     class Meta:
